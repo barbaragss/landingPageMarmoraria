eec802a10eb6970c9b0e1144addd32ef3c32f40c
Bárbara Gabriela <barbaragabi15@hotmail.com>
2021-12-01 23:01:14 -0300

    recebendo valores dos inputs

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const nome = document.getElementById('nome').value;
 
-console.log(nome);
+
+function formulario (){
+    let nome = document.getElementById('nome').value;
+    let telefone = document.getElementById('telefone').value
+    let email = document.getElementById('email').value
+
+    let teste = `nome ${nome} \n telefone ${telefone} \n email ${email}`
+    document.getElementById('teste').innerHTML = teste;
+}
+
+
