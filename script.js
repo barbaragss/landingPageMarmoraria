d03d9b684267310131ebcad143b6ee1d17c07898
Bárbara Gabriela <barbaragabi15@hotmail.com>
2021-12-02 19:43:18 -0300

    validacao do formulario

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 
 
-function formulario (){
-    let nome = document.getElementById('nome').value;
-    let telefone = document.getElementById('telefone').value
-    let email = document.getElementById('email').value
+function enviar (){
 
-    let teste = `nome ${nome} \n telefone ${telefone} \n email ${email}`
-    document.getElementById('teste').innerHTML = teste;
+    let nome = document.getElementById('nome').value;
+    let telefone = document.getElementById('telefone').value;
+    let email = document.getElementById('email').value;
+    let mensagem = document.getElementById ('mensagem').value;
+
+    if(nome != '' && telefone != '' && email != '' && mensagem != ''){
+        let dados = `Seus dados:\nNome: ${nome}\nTelefone: ${telefone}\nEmail: ${email}\nMensagem: ${mensagem}\n\nDados enviados!`
+        //document.getElementById('teste').innerHTML = dados;
+        alert(dados);
+    }
+    else{
+        alert("Preencha os campos!!!");
+    }
+
+
+   
 }
 
+function cancelar(){
+    document.getElementById('nome').value = '' ;
+    document.getElementById('telefone').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('mensagem').value = '';
+}
 
+function validacao (){
+
+}
